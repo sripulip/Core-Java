70156363605459ad9074ef4e77535437b79f8a45
sripulip <psriramkashyap@gmail.com>
2015-06-03 18:33:36 -0400

    Added Lambda Expressions Example

---
+++ src/oopsConcepts/LambdaExpressionsExample.java
@@ -0,0 +1,29 @@
+package oopsConcepts;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class LambdaExpressionsExample {
+
+	public static int calculateSomething(List<Integer> values,
+			Predicate<Integer> selector) {
+
+		return values.stream()
+				.filter(selector)
+				.map(num -> num * 2)
+				.reduce(0, (num, sum) -> num + sum);
+
+	}
+
+	public static void main(String[] args) {
+
+		List<Integer> values = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+		System.out.println(calculateSomething(values, num -> true));
+		System.out.println(calculateSomething(values, num -> num % 2 == 0));
+		System.out.println(calculateSomething(values, num -> num % 2 != 0));
+
+	}
+
+}

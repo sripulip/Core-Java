ddd353ce035c9ed793b3b473e2b280ee05645d12
sripulip <psriramkashyap@gmail.com>
2015-06-04 00:44:35 -0400

    Updated Lambda Expressions Example

--- src/oopsConcepts/LambdaExpressionsExample.java
+++ src/oopsConcepts/LambdaExpressionsExample.java
@@ -6,24 +6,24 @@ import java.util.function.Predicate;
 
 public class LambdaExpressionsExample {
 
-	public static int calculateSomething(List<Integer> values,
-			Predicate<Integer> selector) {
+    public static int calculateSomething(List<Integer> values,
+                                         Predicate<Integer> selector) {
 
-		return values.stream()
-				.filter(selector)
-				.map(num -> num * 2)
-				.reduce(0, (num, sum) -> num + sum);
+        return values.stream()
+                .filter(selector)
+                .map(num -> num * 2)
+                .reduce(0, (num, sum) -> num + sum);
 
-	}
+    }
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		List<Integer> values = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> values = Arrays.asList(1, 2, 3, 4, 5, 6);
 
-		System.out.println(calculateSomething(values, num -> true));
-		System.out.println(calculateSomething(values, num -> num % 2 == 0));
-		System.out.println(calculateSomething(values, num -> num % 2 != 0));
+        System.out.println(calculateSomething(values, num -> true));
+        System.out.println(calculateSomething(values, num -> num % 2 == 0));
+        System.out.println(calculateSomething(values, num -> num % 2 != 0));
 
-	}
+    }
 
 }
